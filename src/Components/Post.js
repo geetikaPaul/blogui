18b286e511a7cf04ecf47154dc889ddb683ee2e4
Paul <PaulGe@Optum.com>
2020-03-31 23:54:19 +0530

    using react-html-render package to render  markdown text

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import './Post.css';
+import renderHTML from 'react-render-html';
 
 class Post extends Component{
 	render(){		
-		console.log(this.props.content.title)
+		console.log(this.props.content.body)
 		return(
 			<div className = "Post">
 				<h3>{this.props.content.title}</h3>
-				<p>{this.props.content.body}
+				<div>{renderHTML(this.props.content.body)}
 				{this.props.showExtendedBody!=this.props.content.id ? <span onClick = {this.props.extendedBodyDisplay}>Read More...</span>: null}
-		  		{this.props.showExtendedBody==this.props.content.id ? <span>{this.props.content.extendedBody}</span>: null}	</p>		
+		  		{this.props.showExtendedBody==this.props.content.id ? <span>{renderHTML(this.props.content.extendedBody)}</span>: null}	</div>		
 			
 			</div>
 		)
